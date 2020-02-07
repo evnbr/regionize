f4ceff15c8aa66ae1e7132352e6fcd00dbb3e169
Evan Brooks <brooks.ev@gmail.com>
2020-02-06 20:11:02 -0800

    attempt to export types

--- rollup.config.js
+++ rollup.config.js
@@ -29,7 +29,7 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
-      typescript(),
+      typescript({ useTsconfigDeclarationDir: true }),
     ],
   }),
 
@@ -75,7 +75,7 @@ export default [
     }),
     plugins: [
       resolve(),
-      typescript(),
+      typescript({ useTsconfigDeclarationDir: true }),
     ],
   }),
 ];
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import Region from './Region';
 import flowIntoRegions from './flowIntoRegions';
 
+
+export declare type ExampleType = 'blah' | 'bleh';
+
 export { Region, flowIntoRegions };
