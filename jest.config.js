c1d830db797a746823dca43cdd0274b70f706add
Evan Brooks <brooks.ev@gmail.com>
2020-02-29 22:51:56 -0800

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,8 @@
 module.exports = {
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.json',
-    },
+    '^.+\\.(ts|tsx)$': 'babel-jest',
   },
   testRegex: '^.+\\.test.ts$',
   testURL: 'http://localhost/',
-  moduleFileExtensions: [
-    'ts',
-    'tsx',
-    'js',
-    'jsx',
-    'json',
-    'node',
-  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
