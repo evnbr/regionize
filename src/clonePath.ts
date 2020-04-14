2039bc41575e5b86a1ec2327c2e9ee857737294d
Evan Brooks <brooks.ev@gmail.com>
2020-04-13 23:11:31 -0700

    Update clonePath.ts

--- src/clonePath.ts
+++ src/clonePath.ts
@@ -1,3 +1,5 @@
+import { SplitRuleApplier } from './types';
+
 // The path is an array of nested elments,
 // for example .content > article > p > a).
 //
@@ -8,7 +10,13 @@
 // The transition can be customized by setting a Split rule,
 // which lets you add classes to the original and cloned element
 // to customize styling.
-import { SplitRuleApplier } from './types';
+
+const clone = (el: HTMLElement, withChildren: boolean) => {
+  return el.cloneNode(withChildren) as HTMLElement;
+};
+
+const shallowClone = (el: HTMLElement) => clone(el, false);
+const deepClone = (el: HTMLElement) => clone(el, true);
 
 const clonePath = (
   oldPath: HTMLElement[],
@@ -16,19 +24,19 @@ const clonePath = (
 ): HTMLElement[] => {
   const newPath = [];
 
-  const deepClone = (el: HTMLElement): HTMLElement => {
-    const clone = el.cloneNode(true) as HTMLElement; // deep clone, could be th > h3 > span;
+  const deepCloneWithRules = (el: HTMLElement): HTMLElement => {
+    const clone = deepClone(el); // could be th > h3 > span;
     applyRules(el, clone);
     return clone;
   };
 
   for (let i = oldPath.length - 1; i >= 0; i -= 1) {
     const original = oldPath[i];
-    const clone = original.cloneNode(false) as HTMLElement; // shallow
+    const clone = shallowClone(original);
     const nextChild = oldPath[i + 1];
     clone.innerHTML = '';
 
-    applyRules(original, clone, nextChild, deepClone);
+    applyRules(original, clone, nextChild, deepCloneWithRules);
 
     if (i < oldPath.length - 1) clone.appendChild(newPath[i + 1]);
     newPath[i] = clone;
