d3d683431da0adb639567b8e4a4266d0bc130a88
Evan Brooks <brooks.ev@gmail.com>
2021-01-02 14:13:20 -0800

    cleanup viewer: hypercript, better indenting

--- viewer/demo.js
+++ viewer/demo.js
@@ -1,45 +1,5 @@
 const { addUntilOverflow } = window.Regionize;
 
-const prettyPrintConfig = (obj) => {
-  const items = Object.keys(obj).map(k => `  ${k}: ${obj[k]}`);
-  return items.length ? `{\n${items.join('\n')}\n}` : '{}';
-};
-
-const setup = async ({ id, name, desc, contentFrag, config }) => {
-  // const item = document.querySelector(`${id}`);
-  const item = document.createElement('section');
-  item.className = 'item';
-  item.setAttribute('id', id);
-
-  const a = document.createElement('a');
-  a.textContent = name;
-  a.setAttribute('href', `#${id}`);
-
-  const h2 = document.createElement('h2');
-  h2.className = 'item-title';
-  h2.append(a);
-
-  const p = document.createElement('p');
-  p.textContent = desc;
-
-  const row = document.querySelector('#row-template').content.cloneNode(true);
-  
-  item.append(h2, p, row);
-
-  const configHolder = item.querySelector('.config-slot');
-  configHolder.append(prettyPrintConfig(config));
-
-  const content = item.querySelector('.content');
-  content.append(contentFrag.cloneNode(true));
-
-  document.body.append(item);
-
-  const region = item.querySelector('.region');
-  const remainderRegion = item.querySelector('.remainder');
-  const result = await addUntilOverflow(content.cloneNode(true), region, config);
-  remainderRegion.append(result.remainder ? result.remainder : '[No remainder]');
-};
-
 const paragraphContent = document.querySelector('#paragraph-content').content;
 const listContent = document.querySelector('#list-content').content;
 const nestedContent = document.querySelector('#nested-content').content;
@@ -58,9 +18,9 @@ const items = [
     desc: 'Using onDidSplit to hide the duplicate indent',
     contentFrag: paragraphContent,
     config: {
-  onDidSplit: (orig, remainder) => {
-    remainder.style.textIndent = 0;
-  },    
+      onDidSplit: (orig, remainder) => {
+        remainder.style.textIndent = 0;
+      },    
     },
   },
   {
@@ -76,11 +36,11 @@ const items = [
     desc: 'Using onDidSplit to hide the duplicate number',
     contentFrag: listContent,
     config: {
-  onDidSplit: (orig, remainder) => {
-    if (remainder.matches('li')) {
-      remainder.style.listStyle = 'none';
-    }
-  },
+      onDidSplit: (orig, remainder) => {
+        if (remainder.matches('li')) {
+          remainder.style.listStyle = 'none';
+        }
+      },
     },
   },
   {
@@ -89,7 +49,7 @@ const items = [
     desc: 'Using canSplit to keep each item whole',
     contentFrag: listContent,
     config: {
-  canSplit: el => !el.matches('li'),
+      canSplit: el => !el.matches('li'),
     },
   },
   {
@@ -102,23 +62,86 @@ const items = [
   {
     id: 'nest2',
     name: 'Styling a split graphical element',
-    desc: 'Using onDidSplit to zero out borders',
+    desc: 'Using onDidSplit to zero out padding and borders. Note that onDidSplit runs after the split point has been determined— you can make arbitrary style changes, but adjusting the space available for text will not reflow.',
     contentFrag: nestedContent,
     config: {
-  onDidSplit: (orig, remainder) => {
-    if (orig.matches('.bordered')) {
-      orig.style.borderBottom = 'none';
-      orig.style.paddingBottom = 0;
-      orig.style.marginBottom = 0;
-      remainder.style.borderTop = 'none';
-      remainder.style.paddingTop = 0;
-      remainder.style.marginTop = 0;
-    }    
-  },
+      onDidSplit: (orig, remainder) => {
+        if (orig.matches('.bordered')) {
+          orig.style.borderBottom = 'none';
+          orig.style.paddingBottom = 0;
+          orig.style.marginBottom = 0;
+          remainder.style.borderTop = 'none';
+          remainder.style.paddingTop = 0;
+          remainder.style.marginTop = 0;
+        }
+      },
     },
   },
 ];
 
-for (const item of items) {
-  setup(item);
-}
+
+const reducedIndent = (obj) => {
+  // everything but the first line of functions has 4 extra indents
+  const lines = obj.toString().split('\n');
+  return lines
+    .map((str, lineIndex) => (lineIndex > 0 ? str.substring(4) : str))
+    .join('\n');
+};
+
+const prettyPrintConfig = (obj) => {
+  const lines = Object
+    .keys(obj)
+    .map(k => `  ${k}: ${reducedIndent(obj[k])}`);
+  return lines.length ? `{\n${lines.join('\n')}\n}` : '{}';
+};
+
+const isNode = input => !!input && input.nodeType;
+const isString = input => !!input && typeof input === 'string';
+const isAppendable = input => isNode(input) || isString(input);
+
+
+const h = (tagName, ...args) => {
+  const el = document.createElement(tagName);
+
+  for (const arg of args) {
+    if (isAppendable(arg)) {
+      el.append(arg);
+    } else {
+      for (const [key, val] of Object.entries(arg)) {
+        if (key in el) {
+          el[key] = val;
+        } else {
+          el.setAttribute(key, val);
+        }
+      }
+    }
+  }
+  return el;
+};
+
+const setup = async ({ id, name, desc, contentFrag, config }) => {
+  const rowFragment = document.querySelector('#row-template').content.cloneNode(true);
+
+  const item = h(
+    'section',
+    { id, className: 'item' },
+    h('h2', { className: 'item-title' }, h('a', { href: `#${id}` }, name)),
+    h('p', desc),
+    rowFragment
+  );
+
+  const configHolder = item.querySelector('.config-slot');
+  configHolder.append(prettyPrintConfig(config));
+
+  const content = item.querySelector('.content');
+  content.append(contentFrag.cloneNode(true));
+
+  document.body.append(item);
+
+  const region = item.querySelector('.region');
+  const remainderRegion = item.querySelector('.remainder');
+  const result = await addUntilOverflow(content.cloneNode(true), region, config);
+  remainderRegion.append(result.remainder ? result.remainder : '[No remainder]');
+};
+
+items.forEach(setup);
